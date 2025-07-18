d13fc2cec8fd32e8bbd46b3c7e0cedc85791e4cf
samaniyusra <yushrasaman92@gmail.com>
2025-07-18 16:13:26 +0530

    changes in about page

--- frontend/src/landing_Page/about/Team.js
+++ frontend/src/landing_Page/about/Team.js
@@ -43,22 +43,22 @@ function Team() {
                         Nithin bootstrapped and founded Zerodha in 2010 to overcome the
                     </p>
                     <p>
-                       hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.
+                        hurdles she faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.
                     </p>
                     <p>
-                        He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).
+                        she is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).
                     </p>
 
                     <p> Playing basketball is his zen.</p>
 
-                    <p> Connect on Homepage / TradingQnA / Twitter</p>
+                    <p > <a style={{textDecoration: "none"}}href="">Connect on Homepage </a> /  <a style={{textDecoration: "none"}}href=""> radingQnA</a>/  <a style={{textDecoration: "none"}}href=""> Twitter</a></p>
                 </div>
             </div>
-</div>
+        </div>
 
 
 
-            )
+    )
 }
 
- export  default Team
+export default Team
